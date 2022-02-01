daef59c7049a14c24ca299781a91d2054f64ca20
ValyaHapka <mironovich0258@gmail.com>
2022-02-01 15:30:41 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  const nAbs = Math.abs(n);
+  const nToArray = Array.from(String(nAbs), Number)
+  const reverseArray = nToArray.reverse();
+  return Number(reverseArray.join(''))
 }
